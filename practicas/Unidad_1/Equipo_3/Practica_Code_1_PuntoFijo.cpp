7980e509473bb59ff3598d9e7c465a448dfbeb83
JoseM-CG <126900511+JoseM-CG@users.noreply.github.com>
2023-03-16 01:28:16 -0600

    Correciones
    Correciones para calcular convergenecia, calcular G y calcular Error. Cambios en función principal, muestra de puntos nuevos y error.

--- practicas/Unidad_1/Equipo_3/Practica_Code_1_PuntoFijo.cpp
+++ practicas/Unidad_1/Equipo_3/Practica_Code_1_PuntoFijo.cpp
@@ -6,7 +6,7 @@ double Error(double [],double []);
 main()
 {
     int k=1;
-    double x[2]={0.2,0.35},x_aux[2]={0};
+    double x[2]={0.2,0.35},x_aux[2]={0},er=0;
     do{
         printf("\nIteracion %d\n",k);
         if(calcConv(x)==false)
@@ -14,31 +14,34 @@ main()
         else
         {
             calcG(x,x_aux);
+            printf("\nx(%d) = %lf\ny(%d) = %lf\n",k,x[0],k,x[1]);
+            er=Error(x,x_aux);
+            printf("\nError = %lf\n",er);
             k++;
         }
-    }while(Error(x,x_aux)>pow(10,-6));
-    printf("\nSolucion:\nx(%d)=(%lf,%lf)",k,x_aux[0],x_aux[1]);
+    }while(er>pow(10,-6));
 }
 bool calcConv(double v[]){
     double a[2]={0};
-    a[0]=abs(sqrt(5));
-    a[1]=(sin(v[0])+4)/(sqrt(-16*pow(v[1],2)+8*v[1]*cos(v[1])+pow(sin(v[1]),2)));
+    a[0]=abs(0)+abs((0.25)*cos(v[0]));
+    a[1]=abs(sqrt(0.2))+abs((-0.25)*sin(v[1]));
     if(a[0]>1||a[1]>1)
     {
-        printf("\nNo converge\n");
+        printf("\nNo converge\na[0] = %lf >= 1\na[1] = %lf >= 1\n",a[0],a[1]);
         return false;
     }
     else
     {
-        printf("\nConverge\n");
+        printf("\nConverge\na[0] = %lf < 1\na[1] = %lf < 1\n",a[0],a[1]);
         return true;
     }
 }
 void calcG(double v[],double v2[]){
-    v2[0]=v[1]*sqrt(5);
-    v2[1]=acos(4*v[1]-cos(v[1]));
-    printf("\nSiguiente punto:\nx=(%lf,%lf)\n",v2[0],v2[1]);
+    v2[0]=v[0];
+    v2[1]=v[1];
+    v[0]=v2[1]*sqrt(0.2);
+    v[1]=(0.25)*(sin(v2[0])+cos(v2[1]));
 }
 double Error(double v[],double v2[]){
-    return sqrt(pow(v2[0]-v[0],2)+pow(v2[1]-v[1],2));
+    return sqrt(pow(v[0]-v2[0],2)+pow(v[1]-v2[1],2));
 }
